4687fe97d43a9af2df0ea15fc034d74c3eeb6715
huangyanqing <huangyanqing@dossen.com>
2019-06-19 09:01:37 +0800

    调整安装脚本地址

--- sprov-ui.sh
+++ sprov-ui.sh
@@ -135,7 +135,7 @@ install_soft() {
 
 install() {
     install_base
-    bash <(curl -L -s https://github.com/sprov065/sprov-ui/raw/master/install.sh)
+    bash <(curl -L -s https://raw.githubusercontent.com/flydoos/sprov-ui/master/install.sh)
     if [[ $? == 0 ]]; then
         if [[ $# == 0 ]]; then
             start
@@ -155,7 +155,7 @@ update() {
         return 0
     fi
     install_base
-    bash <(curl -L -s https://github.com/sprov065/sprov-ui/raw/master/install.sh)
+    bash <(curl -L -s https://raw.githubusercontent.com/flydoos/sprov-ui/master/install.sh)
     if [[ $? == 0 ]]; then
         if [[ $# == 0 ]]; then
             restart
@@ -339,7 +339,7 @@ install_bbr() {
 }
 
 update_shell() {
-    wget -O /usr/bin/sprov-ui -N --no-check-certificate https://github.com/sprov065/sprov-ui/raw/master/sprov-ui.sh
+    wget -O /usr/bin/sprov-ui -N --no-check-certificate https://raw.githubusercontent.com/flydoos/sprov-ui/master/sprov-ui.sh
     if [[ $? != 0 ]]; then
         echo ""
         echo -e "${red}下载脚本失败，请检查本机能否连接 Github${plain}"
